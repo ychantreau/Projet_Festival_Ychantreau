5c92c1aa704bce4cc9a0d95bc7856fb7c28b538d
Sdjurdjevic <>
2017-10-12 14:22:37 +0200

    Ajout d'un lien vers la consultation des groupes.

--- includes/_debut.inc.php
+++ includes/_debut.inc.php
@@ -28,11 +28,11 @@
                     <div id='barreMenus'>
                         <ul class='menus'>
                             <?php construireMenu("Accueil", "index.php", 1); ?>
+                            <?php construireMenu("Consultation Groupes", "cGestionGroupes.php", 6); ?>
                             <?php construireMenu("Gestion établissements", "cGestionEtablissements.php", 2); ?>
                             <?php construireMenu("Gestion types chambres", "cGestionTypesChambres.php", 3); ?>
                             <?php construireMenu("Offre hébergement", "cOffreHebergement.php", 4); ?>
-                            <?php construireMenu("Attribution chambres", "cAttributionChambres.php", 5); ?>
-                            <?php construireMenu("Consultation Groupes", "cGestionGroupes.php", 5); ?>
+                            <?php construireMenu("Attribution chambres", "cAttributionChambres.php", 5); ?>              
                         </ul>
                     </div>
 
