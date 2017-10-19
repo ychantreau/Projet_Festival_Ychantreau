e5004f4bbd3a70c71cd52d26aee33af4af307ccd
ychantreau <ychantreau@btssio-VirtualBox>
2017-10-19 17:24:10 +0200

    Création des scripts pour la table Representations (Create et Insert)

---
+++ sql/Script-Insert-Representations.sql
@@ -0,0 +1,24 @@
+INSERT INTO Representations VALUES ('11/07/2017',2,'g024','19:00','20:00');
+INSERT INTO Representations VALUES ('11/07/2017',3,'g031','11:00','12:00');
+INSERT INTO Representations VALUES ('11/07/2017',3,'g035','12:00','13:00');
+
+INSERT INTO Representations VALUES ('12/07/2017',1,'g008','20:30','22:00');
+INSERT INTO Representations VALUES ('12/07/2017',1,'g009','22:15','23:30');
+
+INSERT INTO Representations VALUES ('13/07/2017',2,'g041','20:30','22:00');
+
+INSERT INTO Representations VALUES ('14/07/2017',1,'g020','19:30','21:00');
+INSERT INTO Representations VALUES ('14/07/2017',1,'g022','21:15','23:00');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g010','14:00','14:30');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g011','14:30','15:00');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g012','15:00','15:30');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g013','15:30','16:00');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g017','16:00','16:30');
+INSERT INTO Representations VALUES ('14/07/2017',3,'g018','16:30','17:00');
+INSERT INTO Representations VALUES ('14/07/2017',4,'g032','11:00','12:00');
+INSERT INTO Representations VALUES ('14/07/2017',4,'g044','15:00','17:00');
+INSERT INTO Representations VALUES ('14/07/2017',4,'g042','17:30','19:30');
+
+INSERT INTO Representations VALUES ('15/07/2017',4,'g037','11:00','12:30');
+INSERT INTO Representations VALUES ('15/07/2017',4,'g025','15:00','16:00');
+INSERT INTO Representations VALUES ('15/07/2017',4,'g029','16:30','19:00');
